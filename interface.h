101f5631c3ab34585d58332e1deb22a840e406a0
Pa1em0n <plmn.c.c@gmail.com>
2015-03-25 03:52:27 -0600

    implementacion de listas

---
+++ interface.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+struct node_struct{
+	int valor;
+	struct node_struct* sig;
+};
+
+struct node_struct * newNode(int v);
+
+void deleteNode(struct node_struct *n);
+
+void print_list(struct node_struct *l);
+
+int longitud_iter(struct node_struct *l);
+
+int longitud_rec(struct node_struct *l);
+
+int maximo(struct node_struct *l);
+
+struct node_struct * inversa(struct node_struct *l);
